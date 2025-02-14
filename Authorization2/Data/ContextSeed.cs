5583e45148c64521f5fa5ca0a3569d408166e05f
MakSimKlim <83maxomus@mail.ru>
2025-02-14 07:17:23 +0500

    User Roles. Seed Default Roles. ContextSeed.cs DONE

---
+++ Authorization2/Data/ContextSeed.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Authorization2.Models;
+
+namespace Authorization2.Data
+{
+    public static class ContextSeed
+    {
+        public static async Task SeedRolesAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Roles
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.SuperAdmin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Moderator.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Basic.ToString()));
+        }
+    }
+}
---
+++ Authorization2/Enums/Roles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authorization2.Enums
+{
+    public enum Roles
+    {
+        SuperAdmin,
+        Admin,
+        Moderator,
+        Basic
+    }
+}
