06dbba68a93c8fff00b06c2e5a1bb28ae68c1eea
MakSimKlim <83maxomus@mail.ru>
2025-02-14 06:48:17 +0500

    Setting a Limit to Change Username DONE

--- Authorization2/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Authorization2/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -39,6 +39,9 @@ namespace Authorization2.Areas.Identity.Pages.Account.Manage
         [TempData]
         public string StatusMessage { get; set; }
 
+        [TempData]
+        public string UserNameChangeLimitMessage { get; set; }
+
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
@@ -70,6 +73,9 @@ namespace Authorization2.Areas.Identity.Pages.Account.Manage
             public string PhoneNumber { get; set; }
             [Display(Name = "Profile Picture")]
             public byte[] ProfilePicture { get; set; }
+
+            //[BindProperty]
+            //public InputModel Input { get; set; }
         }
 
         private async Task LoadAsync(ApplicationUser user)
@@ -97,6 +103,9 @@ namespace Authorization2.Areas.Identity.Pages.Account.Manage
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
+
+            UserNameChangeLimitMessage = $"You can change your username {user.UsernameChangeLimit} more time(s).";
+
 
             // Обработка полей, которые могут быть NULL
             var firstName = user.FirstName ?? "Not provided";
@@ -147,16 +156,41 @@ namespace Authorization2.Areas.Identity.Pages.Account.Manage
                 }
 
             //var profilePicture = user.ProfilePicture;
-                if (Request.Form.Files.Count > 0)
+            if (Request.Form.Files.Count > 0)
+            {
+                IFormFile file = Request.Form.Files.FirstOrDefault();
+                using (var dataStream = new MemoryStream())
                 {
-                    IFormFile file = Request.Form.Files.FirstOrDefault();
-                    using (var dataStream = new MemoryStream())
-                    {
-                        await file.CopyToAsync(dataStream);
-                        user.ProfilePicture = dataStream.ToArray();
-                    }
-                    await _userManager.UpdateAsync(user);
+                    await file.CopyToAsync(dataStream);
+                    user.ProfilePicture = dataStream.ToArray();
                 }
+                await _userManager.UpdateAsync(user);
+            }
+
+            if (user.UsernameChangeLimit > 0)
+            {
+                if (Input.Username != user.UserName)
+                {
+                    var userNameExists = await _userManager.FindByNameAsync(Input.Username);
+                    if (userNameExists != null)
+                    {
+                        StatusMessage = "User name already taken. Select a different username.";
+                        return RedirectToPage();
+                    }
+                    var setUserName = await _userManager.SetUserNameAsync(user, Input.Username);
+                    if (!setUserName.Succeeded)
+                    {
+                        StatusMessage = "Unexpected error when trying to set user name.";
+                        return RedirectToPage();
+                    }
+                    else
+                    {
+                        user.UsernameChangeLimit -= 1;
+                        await _userManager.UpdateAsync(user);
+                    }
+                }
+            }
+
 
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
