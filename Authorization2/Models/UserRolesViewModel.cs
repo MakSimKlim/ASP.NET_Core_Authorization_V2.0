f5ab54995872ac9f080522894f3467839685faa1
MakSimKlim <83maxomus@mail.ru>
2025-02-16 19:29:05 +0500

    Adding Users to Roles DONE

--- Authorization2/Models/UserRolesViewModel.cs
+++ Authorization2/Models/UserRolesViewModel.cs
@@ -11,11 +11,11 @@ namespace Authorization2.Models
 {
     public class UserRolesViewModel
     {
-        public string? UserId { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? UserName { get; set; }
-        public string? Email { get; set; }
-        public IEnumerable<string>? Roles { get; set; }
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<string> Roles { get; set; }
     }
 }
