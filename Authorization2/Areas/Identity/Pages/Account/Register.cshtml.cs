71f5855170ac1e612e1baab5c7e4785d6a0eb380
MakSimKlim <83maxomus@mail.ru>
2025-02-16 16:23:45 +0500

    Add A Default Role to Newly Registered User DONE

--- Authorization2/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Authorization2/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -145,7 +145,7 @@ namespace Authorization2.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
+                    await _userManager.AddToRoleAsync(user, Enums.Roles.Basic.ToString());//Add A Default Role to Newly Registered User
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
